163151c6319855f5c88bda737ea70aca70e86c9e
MarcNumworks <marc.coudriau@numworks.com>
2023-01-16 15:42:14 +0100

    [poincare_junior] Reinterpret float to int with std::bit_cast to preserve constexprness

--- poincare_junior/src/expression/float.h
+++ poincare_junior/src/expression/float.h
@@ -1,19 +1,15 @@
 #ifndef POINCARE_EXPRESSION_FLOAT_H
 #define POINCARE_EXPRESSION_FLOAT_H
 
-#include <utils/bit.h>
+#include <bit>
+#include <omgpj/bit.h>
 
 namespace Poincare {
 
 class Float {
 public:
-  union FloatMemory {
-    float m_float;
-    uint32_t m_int;
-  };
   constexpr static uint8_t SubFloatAtIndex(float value, int index) {
-    FloatMemory f = {.m_float = value};
-    return Bit::getByteAtIndex(f.m_int, index);
+    return Bit::getByteAtIndex(std::bit_cast<uint32_t>(value), index);
   }
 };
 
