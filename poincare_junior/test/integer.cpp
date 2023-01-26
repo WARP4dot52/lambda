fd33ecb6ce392966d0179df5af131b469409ef0d
Emilie Feral <emilie.feral@numworks.com>
2023-01-26 13:44:59 +0100

    [poincare_junior] Add tests on Integer

--- poincare_junior/test/integer.cpp
+++ poincare_junior/test/integer.cpp
@@ -2,8 +2,22 @@
 #include <poincare_junior/src/expression/integer.h>
 #include <poincare_junior/src/expression/rational.h>
 #include <poincare_junior/src/memory/tree_constructor.h>
+#include <quiz.h>
 
 using namespace PoincareJ;
+
+static inline IntegerHandler CreateIntegerHandler(const char * digits) {
+  size_t length = strlen(digits);
+  OMG::Base base = OMG::Base::Decimal;
+  size_t prefixLength = strlen("0b");
+  assert(prefixLength == strlen("0x"));
+  if (length > prefixLength && digits[0] == '0' && (digits[1] == 'b' || digits[1] == 'x')) {
+    base = digits[1] == 'b' ? OMG::Base::Binary : OMG::Base::Hexadecimal;
+    digits = digits + prefixLength;
+    length -= prefixLength;
+  }
+  return Integer::Handler(Integer::Push(digits, length, base));
+}
 
 QUIZ_CASE(pcj_integer_constructor) {
   IntegerHandler zero(static_cast<uint8_t>(0));
@@ -28,8 +42,9 @@ QUIZ_CASE(pcj_integer_properties) {
   IntegerHandler one(1);
   IntegerHandler two(2);
   IntegerHandler minusOne(-1);
-  IntegerHandler a = Integer::Handler(Integer::Push("254", sizeof("254") - 1));
-  IntegerHandler b = Integer::Handler(Integer::Push("-12", sizeof("-12") - 1));
+  IntegerHandler a = CreateIntegerHandler("254");
+  IntegerHandler b = CreateIntegerHandler("-13");
+
   assert(a.strictSign() == StrictSign::Positive);
   assert(b.strictSign() == StrictSign::Negative);
   assert(zero.strictSign() == StrictSign::Null);
@@ -37,11 +52,46 @@ QUIZ_CASE(pcj_integer_properties) {
   assert(!a.isOne() && one.isOne());
   assert(!a.isMinusOne() && minusOne.isMinusOne());
   assert(!a.isTwo() && two.isTwo());
-  assert(!a.isSignedType<int8_t>() && b.isSignedType<int8_t>() && static_cast<int8_t>(b) == -12);
+  assert(a.isEven() && !b.isEven());
+  assert(!a.isSignedType<int8_t>() && b.isSignedType<int8_t>() && static_cast<int8_t>(b) == -13);
   assert(!b.isUnsignedType<uint8_t>() && a.isUnsignedType<uint8_t>() && static_cast<uint8_t>(a) == 254);
 }
 
-QUIZ_CASE(pcj_integer_compare) {}
+static inline void assert_equal(const char * a, const char * b) {
+  quiz_assert(IntegerHandler::Compare(CreateIntegerHandler(a), CreateIntegerHandler(b)) == 0);
+}
+static inline void assert_not_equal(const char * a, const char * b) {
+  quiz_assert(IntegerHandler::Compare(CreateIntegerHandler(a), CreateIntegerHandler(b)) != 0);
+}
+
+static inline void assert_lower(const char * a, const char * b) {
+  quiz_assert(IntegerHandler::Compare(CreateIntegerHandler(a), CreateIntegerHandler(b)) < 0);
+}
+
+static inline void assert_greater(const char * a, const char * b) {
+  quiz_assert(IntegerHandler::Compare(CreateIntegerHandler(a), CreateIntegerHandler(b)) > 0);
+}
+
+QUIZ_CASE(pcj_integer_compare) {
+  assert_equal("123", "123");
+  assert_equal("-123", "-123");
+  assert_not_equal("-123", "123");
+  assert_equal("1234567891011121314", "1234567891011121314");
+  assert_equal("1234567891011121314", "1234567891011121314");
+  assert_not_equal("-1234567891011121314", "1234567891011121314");
+  assert_lower("123", "456");
+  assert_greater("456", "123");
+  assert_lower("-100", "2");
+  assert_lower("-200", "-100");
+  assert_lower("123", "123456789123456789");
+  assert_lower("-123", "123456789123456789");
+  assert_lower("123456789123456788", "123456789123456789");
+  assert_lower("-1234567891234567892109209109", "123456789123456789");
+  assert_greater("123456789123456789", "123456789123456788");
+  assert_equal("0x2BABE", "178878");
+  assert_equal("0b1011", "11");
+}
+
 QUIZ_CASE(pcj_integer_addition) {}
 #if 0
 static inline void assert_equal(const IntegerHandler i, const IntegerHandler j) {
