9e726cfd9411760e0713fc6351f801026bb06c48
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-04-13 11:58:54 +0200

    [poincare_junior/test] Fix pcj_layout_creation

--- poincare_junior/test/layout.cpp
+++ poincare_junior/test/layout.cpp
@@ -11,18 +11,18 @@ using namespace PoincareJ;
 
 QUIZ_CASE(pcj_layout_creation) {
   // text -> Layout
-  Layout l1 = Layout::Parse("-1+2*3");
+  Layout l1 = Layout::Parse("1+2*3");
   // text -> Expression -> Layout
-  Expression e1 = Expression::Parse("-1+2*3");
+  Expression e1 = Expression::Parse("1+2*3");
   Layout l2 = e1.toLayout();
   assert(l2.treeIsIdenticalTo(l1));
   // expression node -> Expression -> Layout
-  Expression e2 = Expression(KAdd(-1_e, KMult(2_e, 3_e)));
+  Expression e2 = Expression(KAdd(1_e, KMult(2_e, 3_e)));
   Layout l3 = e2.toLayout();
   // layout Node -> Layout
   assert(l3.treeIsIdenticalTo(l1));
   // constexpr tree -> Layout
-  Layout l4 = Layout("-1+2*3"_l);
+  Layout l4 = Layout("1+2*3"_l);
   assert(l4.treeIsIdenticalTo(l1));
 }
 
