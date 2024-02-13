2efd74b77cdf85a2564add9c1fc3b775d64a078a
Pia Chancerel <pia.chancerel@numworks.com>
2024-02-13 16:21:17 +0100

    docs: enhance comment

--- apps/shared/curve_view_range.cpp
+++ apps/shared/curve_view_range.cpp
@@ -56,41 +56,41 @@ float CurveViewRange::computeGridUnit(Axis axis) const {
   /* Proof of the algorithm:
    *
    * We want to find gridUnit = a*10^b, with a in {1; 2; 5} and b an integer
-   * We want: minNumberOfUnits <= range/gridUnit < maxNumberOfUnits
+   * We want: minNumberOfUnits <= range / gridUnit <= maxNumberOfUnits
    *
    * A solution thus needs to verify:
    *
-   *        minNumberOfUnits/range <= 1/(a*10^b) < maxNumberOfUnits/range
-   * =>     range/minNumberOfUnits >= a*10^b > range/maxNumberOfUnits
-   * =>     range/(a*minNumberOfUnits) >= 10^b > range/(a*maxNumberOfUnits)
-   * =>     log10(range/(a*minNumberOfUnits)) >= b > log10(range/(a*maxNumberOfUnits))
-   * => (1) log10(range/(a*maxNumberOfUnits)) < b <= log10(range/(a*minNumberOfUnits))
+   *                            minNumberOfUnits <= range/(a*10^b) <= maxNumberOfUnits
+   * <=>                minNumberOfUnits / range <=   1/(a*10^b)   <= maxNumberOfUnits / range
+   * <=>                range / minNumberOfUnits >=     a*10^b     >= range / maxNumberOfUnits
+   * <=>          range / (a * minNumberOfUnits) >=      10^b      >= range / a * maxNumberOfUnits
+   * <=>   log10(range / (a * minNumberOfUnits)) >=        b       >= log10(range / a * maxNumberOfUnits)
+   * <=>   log10(range / (a * maxNumberOfUnits)) <=        b       <= log10(range / a * minNumberOfUnits)    (1)
+   *
    * And, because b must be an integer,
-   * =>     floor(log10(range/(a*maxNumberOfUnits))) != floor(log10(range/(a*minNumberOfUnits)))
-   * The solution is then b = floor(log10(range/(a*minNumberOfUnits)))
+   * <=>   ceil(log10(range / (a * maxNumberOfUnits))) <= b <= floor(log10(range / a * minNumberOfUnits))
+   *
+   * To have a solution, we must have
+   *          ceil(log10(range / (a * maxNumberOfUnits))) <= floor(log10(range / a * minNumberOfUnits))
+   * <=>     floor(log10(range / (a * maxNumberOfUnits))) <  floor(log10(range / a * minNumberOfUnits))
+   * <=>     floor(log10(range / (a * maxNumberOfUnits))) != floor(log10(range / a * minNumberOfUnits))
+   *
+   * A solution is then b = floor(log10(range / (a * minNumberOfUnits)))
    *
    * Is there always at least one solution ?
    *
    * (1) also gives:
-   * E1 = log10(range)-log10(a)-log10(maxNumberOfUnits) < b <= log10(range)-log10(a)-log10(maxNumberOfUnits) = E2
+   * E1(a) = log10(range)-log10(a)-log10(maxNumberOfUnits) <= b <= log10(range)-log10(a)-log10(minNumberOfUnits) = E2(a)
    *
-   * Let's compute E2-E1:
-   * E2-E1 = log10(maxNumberOfUnits) - log10(minNumberOfUnits)
-   * For minNumberOfUnits=7 and maxNumberOfUnits=18, E2-E1 = 0.41...
-   * For minNumberOfUnits=5 and maxNumberOfUnits=13, E2-E1 = 0.41...
+   * Let's compute E2(a)-E1(a):
+   * E2(a)-E1(a) = log10(maxNumberOfUnits) - log10(minNumberOfUnits)
+   * For minNumberOfUnits=7 and maxNumberOfUnits=18, E2(a)-E1(a) = 0.41...
+   * For minNumberOfUnits=5 and maxNumberOfUnits=13, E2(a)-E1(a) = 0.41...
    *
-   * Let's compute the union of the [E1;E2] for a in {1; 2; 5}:
-   * [E1;E2 for a=1] U [E1;E2 for a=2] U [E1;E2 for a=5]
-   *  = [e1;e2] U [e1-log10(2); e2-log10(2)] U [e1-log10(5); e2-log10(5)]
-   *  = [e1;e2] U [e1-0.3; e2-0.3] U [e1-0.7; e2-0.7]
-   *  = [e1-0.7; e2-0.7] U [e1-0.3; e2-0.3] U [e1;e2]
-   *  = [e1-0.7; e2]  because e2-0.7 > e1-0.3 as e2-e1 > 0.7-0.3 and e2-e1 = E2-E1 = 0.41...
-   *                  and  e2-0.3 > e1 as e2-e1 > 0.3
-   *
-   * The union of the [E1;E2] for a in {1; 2; 5} is an interval of size
-   * e2-e1+0.7 = 1.1 > 1.
-   * We will thus always have at least one a in {1; 2; 5} for which E1 and E2
-   * are on each side of an integer.
+   * Let e1 = log10(range / maxNumberOfUnits) and e2 = log10(range / minNumberOfUnits)
+   * [E1(1); E2(1)] = [e1-log10(1); e2-log10(1)] = [e1; e2]
+   * [E1(2); E2(2)] = [e1-log10(2); e2-log10(2)] = [e1-0.3; e2-0.3]
+   * [E1(5); E2(5)] = [e1-log10(5); e2-log10(5)] = [e1-0.7; e2-0.7]
    *
    * Let's make a drawing.
    *
@@ -105,6 +105,11 @@ float CurveViewRange::computeGridUnit(Axis axis) const {
    * --------------------------------------------------------------------------------------------
    *
    * If e1 - floor(e1) > 1-(e2-e1) = 0.58..., a=1 is a solution
+   * Proof:
+   * e1 - floor(e1) > 1-(e2-e1) <=> floor(e1) < e2 -1
+   *                            <=> floor(e1) <= floor(e2-1) = floor(e2)-1
+   *                            <=> floor(e1) < floor(e2)
+   *                            <=> floor(E1(1)) < floor(E2(1))
    *
    *                  n       n+0.2     n+0.4     n+0.6     n+0.8       n+1
    *        .........||....|....|....|....|....|....|....|....|....|....||...  a=1
@@ -113,7 +118,15 @@ float CurveViewRange::computeGridUnit(Axis axis) const {
    * --------------------------------------------------------------------------------------------
    * --------------------------------------------------------------------------------------------
    *
-   * If log10(5)-(e2-e1) = 0.29... < e1 - floor(e1) < log10(5) = 0.69..., a=5 is a solution
+   * If log10(5) - (e2 - e1) = 0.29... < e1 - floor(e1) < log10(5) = 0.69..., a=5 is a solution
+   * Proof:
+   * 1) log10(5) - (e2 - e1) < e1 - floor(e1) <=> log10(5) + e1 - e2 < e1 - floor(e1)
+   *                                       <=> log10(5) + e1 - e2 < e1 - floor(e1)
+   *                                       <=> floor(e1) < e2 - log10(5)
+   * 2) e1 - floor(e1) < log10(5) <=> e1 - log(5) < floor(e1)
+   * 1) + 2) => e1 - log(5) < floor(e1) < e2 - log10(5)
+   *         => floor(e1 - log(5)) < floor(e2 - log10(5))
+   *         => floor(E1(5)) < floor(E2(5))
    *
    *                  n       n+0.2     n+0.4     n+0.6     n+0.8       n+1
    *        .........||....|....|....|....|....|....|....|....|....|....||...
@@ -127,6 +140,13 @@ float CurveViewRange::computeGridUnit(Axis axis) const {
    * --------------------------------------------------------------------------------------------
    *
    * If e1 - floor(e1) < log10(2) = 0.3..., a=2 is a solution
+   * Proof:
+   * 1) log10(2) - (e2 - e1) < 0 and e1 - floor(e1) > 0 => log10(2) - (e2 - e1) < e1 - floor(e1)
+   * 2) e1 - floor(e1) < log10(2) <=> e1 - log(2) < floor(e1)
+   * 1) + 2) => e1 - log(2) < floor(e1) < e2 - log10(2)
+   *         => floor(e1 - log(2)) < floor(e2 - log10(2))
+   *         => floor(E1(2)) < floor(E2(2))
+   *
    *                  n       n+0.2     n+0.4     n+0.6     n+0.8       n+1
    *        .........||....|....|....|....|....|....|....|....|....|....||...
    *                          e1^--------------------^e2
