c7b52224c3c58541f65d019e2fbf6a3b4eff2caf
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2024-03-12 10:51:36 +0100

    [pcj/layout] Handle NthRoot Layout

--- poincare/src/junior_expression.cpp
+++ poincare/src/junior_expression.cpp
@@ -265,6 +265,8 @@ ExpressionNode::Type JuniorExpression::type() const {
       // TODO_PCJ: Handle symbols better
       return ExpressionNode::Type::JuniorExpression;
       // Unused in apps, but they should not raise the default assert.
+    case PoincareJ::BlockType::NthRoot:
+      return ExpressionNode::Type::NthRoot;
     case PoincareJ::BlockType::SquareRoot:
       return ExpressionNode::Type::SquareRoot;
     case PoincareJ::BlockType::Subtraction:
