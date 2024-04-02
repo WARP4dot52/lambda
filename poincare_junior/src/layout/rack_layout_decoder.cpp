9d4a2ea209c3fdbdab02e59485199988ae124f04
MarcNumworks <marc.coudriau@numworks.com>
2024-04-02 11:00:22 +0200

    [pcj] Add comment

--- poincare_junior/src/layout/rack_layout_decoder.cpp
+++ poincare_junior/src/layout/rack_layout_decoder.cpp
@@ -7,7 +7,11 @@ CodePoint CPLayoutDecoder::codePointAt(size_t index) const {
     return UCodePointNull;
   }
   assert(0 <= index && index < m_end);
-
+  /* This was previously m_firstCodePoint + index * k_codePointLayoutSize; but
+   * was changed since codepoints are of varying sizes. It is less efficient but
+   * maybe having the CPLayoutDecoder limited to ascii codepoints is
+   * sufficient. We will probably never need to parse non-ascii symbols like π
+   * as part of a word. Decoders needs to be refactored anyway. */
   const Tree* codePoint = m_firstCodePoint;
   for (int i = 0; i < index; i++) {
     codePoint = codePoint->nextTree();
