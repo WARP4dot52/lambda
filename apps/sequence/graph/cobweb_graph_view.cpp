9173b12a18796faf900e8a17bba606a73f0ef726
MarcNumworks <marc.coudriau@numworks.com>
2024-06-03 11:21:55 +0200

    [sequence] Factor k_unknownName

--- apps/sequence/graph/cobweb_graph_view.cpp
+++ apps/sequence/graph/cobweb_graph_view.cpp
@@ -67,8 +67,7 @@ void CobwebPlotPolicy::drawPlot(const AbstractPlotView *plotView,
       name, strlen(name), Poincare::Symbol::SystemSymbol());
   Poincare::Symbol variable = Poincare::Symbol::SystemSymbol();
   function = function.replaceSymbolWithExpression(sequenceSymbol, variable);
-  constexpr static char k_unknownName[2] = {UCodePointUnknown, 0};
-  function = function.getSystemFunction(k_unknownName);
+  function = function.getSystemFunction(Shared::Function::k_unknownName);
   Curve2DEvaluation<float> evaluateFunction = [](float t, void *model,
                                                  void *context) {
     Poincare::SystemFunction *e = (Poincare::SystemFunction *)model;
