812b2469055eeca88deff42e8c773f7ceaa31c1a
MarcNumworks <marc.coudriau@numworks.com>
2024-06-27 18:24:08 +0200

    fix(omg): add assert(false) in unreachable to get a message in DEBUG

--- omg/include/omg/unreachable.h
+++ omg/include/omg/unreachable.h
@@ -1,12 +1,17 @@
 #ifndef OMG_UNREACHABLE_H
 #define OMG_UNREACHABLE_H
 
+#include "assert.h"
+
 namespace OMG {
 
 /* Raise an undefined behavior, equivalent to C++23 std::unreachable.
  * Use it instead of assert(false) to mark places that should not be reached.
  */
-[[noreturn]] inline void unreachable() { __builtin_unreachable(); }
+[[noreturn]] inline void unreachable() {
+  assert(false);
+  __builtin_unreachable();
+}
 
 }  // namespace OMG
 
