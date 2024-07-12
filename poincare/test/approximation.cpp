3b6464ec249a9e77ffe715f74025d41aef83a7aa
MarcNumworks <marc.coudriau@numworks.com>
2024-07-12 18:09:35 +0200

    test: add broken tests

--- poincare/test/approximation.cpp
+++ poincare/test/approximation.cpp
@@ -192,8 +192,10 @@ QUIZ_CASE(pcj_approximation_infinity) {
   approximates_to<float>("atan(-inf)", "-1.570796");
 
   // nonreal vs undef
+  approximates_to<float>("nonreal", "undef");  // TODO: nonreal
   approximates_to<float>("√(-1)", "nonreal");
   approximates_to<float>("√(-1)+1/0", "undef");
+  approximates_to<float>("1/(√(-1)^2+1)", "undef");  // TODO: nonreal
   approximates_to<float>("{√(-1),1/0}", "{nonreal,undef}");
   approximates_to<float>("(√(-1),2)", "(undef,undef)");  // TODO: (nonreal,2)
   approximates_to<float>("(1/0,2)", "(undef,2)");
