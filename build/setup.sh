eddbac17d86c8545d64554e1c3448b17ce535754
LoreneNumworks <lorene.imperor@numworks.com>
2024-08-19 14:39:51 +0200

    feature(ci/setup): add debian testing to package sources

--- build/setup.sh
+++ build/setup.sh
@@ -74,6 +74,11 @@ install_macos_binary_deps() {
 }
 
 install_linux_binary_deps() {
+  # add the "debian testing" sources
+  if ! [ -f "/etc/apt/sources.list.d/debian-testing.list" ]; then
+    sudo bash -c "echo deb http://deb.debian.org/debian testing main >> /etc/apt/sources.list.d/debian-testing.list"
+  fi
+  sudo apt update && sudo apt upgrade
   sudo apt-get install \
     build-essential \
     clang-format \
