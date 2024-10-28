a18eb6cf27698b7238b6c64d66c8e37a56174dc4
Pia Chancerel <pia.chancerel@numworks.com>
2024-10-28 11:12:34 +0100

    style(expression): rename multiplication in e for the sake of simplicity

--- poincare/src/expression/systematic_multiplication.cpp
+++ poincare/src/expression/systematic_multiplication.cpp
@@ -142,65 +142,62 @@ static bool ReduceMultiplicationWithInf(Tree* e) {
   return false;
 }
 
-static bool SimplifySortedMultiplication(Tree* multiplication) {
-  assert(multiplication->isMult());
-  int n = multiplication->numberOfChildren();
+static bool SimplifySortedMultiplication(Tree* e) {
+  assert(e->isMult());
+  int n = e->numberOfChildren();
   bool changed = false;
   assert(n > 1);
   /* Recursively merge children.
    * Keep track of n and changed status. */
   int numberOfDependencies = 0;
   TreeRef depList = SharedTreeStack->pushDepList(numberOfDependencies);
-  ReduceMultiplicationChildRec(multiplication->child(0), 0, &n, &changed,
+  ReduceMultiplicationChildRec(e->child(0), 0, &n, &changed,
                                &numberOfDependencies);
   assert(n > 0);
-  NAry::SetNumberOfChildren(multiplication, n);
+  NAry::SetNumberOfChildren(e, n);
   if (numberOfDependencies > 0) {
     NAry::SetNumberOfChildren(depList, numberOfDependencies);
     // TODO: create moveTreeAfterNode
-    multiplication->nextTree()->moveTreeBeforeNode(depList);
-    multiplication->cloneNodeAtNode(KDep);
-    multiplication = multiplication->nextNode();
+    e->nextTree()->moveTreeBeforeNode(depList);
+    e->cloneNodeAtNode(KDep);
+    e = e->nextNode();
   } else {
     depList->removeTree();
   }
 
-  if (multiplication->child(0)->isZero()) {
-    Dimension dim = Dimension::Get(multiplication);
+  if (e->child(0)->isZero()) {
+    Dimension dim = Dimension::Get(e);
     if (dim.isUnit()) {
       // 0 * 0 * 2 * (m + km) * m -> 0 * m^2
       // Use hash because change is too complex to track.
-      uint32_t hash = changed ? 0 : multiplication->hash();
+      uint32_t hash = changed ? 0 : e->hash();
       if (dim.hasNonKelvinTemperatureUnit()) {
         /* Temperature exception : 0*_°C != 0*K : unit must be preserved.
          * Taking advantage of the fact only very simple expressions of such
          * temperatures are allowed. */
         assert(dim.unit.vector.supportSize() == 1 &&
                dim.unit.vector.temperature == 1);
-        multiplication->moveTreeOverTree(
-            Units::Unit::Push(dim.unit.representative));
-        multiplication->moveTreeOverTree(
-            PatternMatching::Create(KMult(0_e, KA), {.KA = multiplication}));
+        e->moveTreeOverTree(Units::Unit::Push(dim.unit.representative));
+        e->moveTreeOverTree(PatternMatching::Create(KMult(0_e, KA), {.KA = e}));
       } else {
         // Since all units are equivalent, use base SI.
-        multiplication->moveTreeOverTree(
-            Units::Unit::GetBaseUnits(dim.unit.vector));
-        if (multiplication->isMult()) {
-          NAry::Sort(multiplication, Order::OrderType::PreserveMatrices);
+        e->moveTreeOverTree(Units::Unit::GetBaseUnits(dim.unit.vector));
+        if (e->isMult()) {
+          NAry::Sort(e, Order::OrderType::PreserveMatrices);
         } else {
-          multiplication->cloneNodeAtNode(KMult.node<1>);
+          e->cloneNodeAtNode(KMult.node<1>);
         }
-        NAry::AddChildAtIndex(multiplication, (0_e)->cloneTree(), 0);
+        NAry::AddChildAtIndex(e, (0_e)->cloneTree(), 0);
       }
-      return changed || (hash != multiplication->hash());
+      return changed || (hash != e->hash());
     }
   }
 
-  if (changed && NAry::SquashIfPossible(multiplication)) {
+  if (changed && NAry::SquashIfPossible(e)) {
     return true;
   }
 
-  if (ReduceMultiplicationWithInf(multiplication)) {
+  if (ReduceMultiplicationWithInf(e)) {
     changed = true;
   }
 
@@ -211,8 +208,8 @@ static bool SimplifySortedMultiplication(Tree* multiplication) {
   /* Merging children can un-sort the multiplication. It must then be simplified
    * again once sorted again. For example:
    * 3*a*i*i -> Simplify -> 3*a*-1 -> Sort -> -1*3*a -> Simplify -> -3*a */
-  if (NAry::Sort(multiplication, Order::OrderType::PreserveMatrices)) {
-    SimplifySortedMultiplication(multiplication);
+  if (NAry::Sort(e, Order::OrderType::PreserveMatrices)) {
+    SimplifySortedMultiplication(e);
   }
   return true;
 }
