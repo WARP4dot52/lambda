825366bf7d22dba5df7ba3f89ed5f250df3b8131
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2025-02-04 15:33:01 +0100

    refactor: improve liba/limits.h

--- liba/include/limits.h
+++ liba/include/limits.h
@@ -2,8 +2,8 @@
 #define LIBA_LIMITS_H
 
 #define CHAR_BIT 8
-#define INT_MAX 0x7fffffff
-#define INT_MIN (-INT_MAX - 1)
-#define UINT_MAX 0xffffffff
+#define INT_MAX 0x7fffffff            //  2147483647 = 0x7fffffff
+#define INT_MIN (-INT_MAX - 1)        // –2147483648 = 0x80000000
+#define UINT_MAX (INT_MAX * 2U + 1U)  //  4294967295 = 0xffffffff
 
 #endif
