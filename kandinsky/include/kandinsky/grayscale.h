63faed9aca5de031fa46c5f1ba0fc516c6737bfa
MarcNumworks <marc.coudriau@numworks.com>
2025-03-12 10:14:19 +0100

    refactor(scandium): move Hiero::UI::Color to kandinsky

--- kandinsky/include/kandinsky/grayscale.h
+++ kandinsky/include/kandinsky/grayscale.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <omg/unreachable.h>
 #include <stdint.h>
+
+#include "color.h"
 
 enum class KDGrayscale : uint8_t {
   Darkest = 0,
@@ -9,3 +12,18 @@ enum class KDGrayscale : uint8_t {
   Brightest,
   NumberOfShades,
 };
+
+constexpr KDColor KDGrayscaleToKDColor(KDGrayscale color) {
+  switch (color) {
+    case KDGrayscale::Darkest:
+      return KDColorBlack;
+    case KDGrayscale::Dark:
+      return KDColor::RGB888(0x32, 0x34, 0x32);
+    case KDGrayscale::Bright:
+      return KDColor::RGB888(0x66, 0x6D, 0x68);
+    case KDGrayscale::Brightest:
+      return KDColor::RGB888(0x88, 0x96, 0x8C);
+    default:
+      OMG::unreachable();
+  }
+}
