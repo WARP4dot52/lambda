d6f9c9b2fec1bfd26d9b170c04b43d502982f35c
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2025-05-14 11:32:06 +0200

    build: format .c ion files

--- ion/src/device/shared/boot/ssp.c
+++ ion/src/device/shared/boot/ssp.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 // __stack_chk_guard value initialization is done at platform initialization
-__attribute__((used,section(".canary"))) uintptr_t __stack_chk_guard;
+__attribute__((used, section(".canary"))) uintptr_t __stack_chk_guard;
 
-__attribute__((noreturn,used)) void __stack_chk_fail() {
-  abort();
-}
+__attribute__((noreturn, used)) void __stack_chk_fail() { abort(); }
