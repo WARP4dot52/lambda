cb067be6618d76849ac106179eff456baf7c494b
Félix Richart <felix.richart@numworks.com>
2025-05-27 16:26:18 +0200

    refactor(external_apps): search for emsdk version at epsilon root

--- external_apps/setup.ps1
+++ external_apps/setup.ps1
@@ -1,6 +1,13 @@
 winget install --accept-package-agreements python
 winget install ezwinports.make
+
+$emsdkVersion = if (Test-Path ../.emsdk-version) {
+    Get-Content ../.emsdk-version | Out-String | ForEach-Object { $_.Trim() }
+} else {
+    "latest"
+}
+
 git clone https://github.com/emscripten-core/emsdk.git
-./emsdk/emsdk install 4.0.8
-./emsdk/emsdk activate 4.0.8
+./emsdk/emsdk install $emsdkVersion
+./emsdk/emsdk activate $emsdkVersion
 npm install -g nwlink
--- external_apps/setup.sh
+++ external_apps/setup.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+# Try to read version from ../.emsdk-version
+if [ -f ../.emsdk-version ]; then
+  EMSDK_VERSION=$(cat ../.emsdk-version)
+else
+  # Fallback to default version if file does not exist
+  EMSDK_VERSION="latest"
+fi
+
 git clone https://github.com/emscripten-core/emsdk.git
-./emsdk/emsdk install 4.0.8
-./emsdk/emsdk activate 4.0.8
+./emsdk/emsdk install $EMSDK_VERSION
+./emsdk/emsdk activate $EMSDK_VERSION
 npm install -g nwlink
